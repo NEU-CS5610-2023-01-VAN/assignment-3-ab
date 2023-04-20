cab5c922250d106553424479564ed48cc55d939a
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-04-20 15:38:23 -0700

    added Routes, created NavBar

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import VerifyUser from "./pages/SignIn";
+import UserPosts from "./pages/UserPosts";
+import UserDetail from "./pages/UserDetail";
+import PostDetails from "./pages/PostDetails";
+import NotFound from "./pages/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: `${window.location.origin}/verify-user`,
+      audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+      scope: requestedScopes.join(" "),
+    }}
+  >
+      <AuthTokenProvider> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/verify-user" element={<VerifyUser />} />
+        <Route path="user/:id/posts" element={<UserPosts />} />
+        <Route path="users/:id" element={<UserDetail />} />
+        <Route path="posts/:id" element={<PostDetails />} />
+        {/* <Route path="search/:search" element={<SearchResult />} /> */}
+
+        {/* <Route path="profile" element={<ProfileDetail />} />
+        <Route path="posts/new" element={<EditPost />} />
+        <Route path="posts/edit/:id" element={<EditPost />} />
+        <Route path="profile/settings" element={<ProfileSettings />} /> */}
+
+        {/* <Route path="debugger" element={<AuthDebugger />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    {/* </AuthTokenProvider>
+    </Auth0Provider> */}
   </React.StrictMode>
 );
 
