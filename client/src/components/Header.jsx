6cf5fb59db22e1c61d3888a17a1c44af6b876f66
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-05-13 02:27:11 -0700

    changed tab names and tab icon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
 
   const navigation = [
     { name: "Home", to: "/", current: false },
-    { name: "ExternalAPI", to: "/externalAPI", current: false },
+    { name: "External Resources", to: "/externalAPI", current: false },
   ];
   const userNavigation = isAuthenticated
     ? [
@@ -65,7 +65,7 @@ export default function Navbar() {
                       disabled={true}
                       id="search"
                       name="search"
-                      className="block w-full rounded-md border-0 bg-gray-700 py-1.5 pl-10 pr-3 text-gray-300 placeholder:text-gray-400 focus:bg-white focus:text-gray-900 focus:ring-0 focus:placeholder:text-gray-500 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 bg-gray-700 py-1.5 pl-10 pr-3 text-gray-300 placeholder:text-gray-400 focus:bg-white focus:text-gray-900 focus:ring-0 focus:placeholder:text-gray-500 sm:text-sm sm:leading-6 cursor-not-allowed"
                       placeholder="Search"
                       type="search"
                     />
